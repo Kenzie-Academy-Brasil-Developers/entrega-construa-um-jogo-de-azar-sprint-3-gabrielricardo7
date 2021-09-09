79bc48f042b7f1f72528aeabf26d19f93930fbca
Gabriel Ricardo <gabrielricardo7@gmail.com>
2021-09-09 16:26:45 -0300

    Add main.js file

---
+++ main.js
@@ -0,0 +1,11 @@
+let response;
+switch (response) {
+    case 0:
+        console.log("Don't count on it");
+        break;
+    case 1:
+        console.log("Yes");
+        break;
+    default:
+        console.log("Ask again later");
+}
